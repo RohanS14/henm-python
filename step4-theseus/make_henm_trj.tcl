6a8f8fc1f3d8caa7f6ba27f74ee3792c4543f6e9
RohanS14 <rohan@subramanianfamily.com>
2024-11-06 09:43:10 -0800

    updated mdanalysis stuff

--- step4-theseus/make_henm_trj.tcl
+++ step4-theseus/make_henm_trj.tcl
@@ -1,7 +1,8 @@
 # This is a VMD script
 
 # Load the data (gro file and traj file)
-mol load gro pfpatch_000000001669_protein.gro trr pfpatch_000000001669_protein_aligned.trr
+# mol load gro pfpatch_000000001669_protein.gro trr pfpatch_000000001669_protein_aligned.trr
+mol load gro protein_initial_unaligned.gro trr protein_bb_unaligned_pbc_fixed.trr
 
 # Renaming BB (backbone) to CA (carbon alpha) for Martini
 [atomselect top "name BB"] set name CA
@@ -11,4 +12,4 @@ mol load gro pfpatch_000000001669_protein.gro trr pfpatch_000000001669_protein_a
 set sel [atomselect top "name CA"]
 
 # writing the selected and renamed stuff to the new file
-animate write pdb pfpatch_000000001669_rasraf_new.pdb beg 1 end -1 waitfor all sel $sel
+animate write pdb pfpatch_000000001669_rasraf_new_from_unaligned.pdb beg 1 end -1 waitfor all sel $sel
