290ee3d57e6ea6150fc7dba0c9f739db3ca5f604
Olivia Schleifer <oschleifer@hmc.edu>
2024-12-08 17:18:34 -0800

    lammps-input added

--- run-all.sh
+++ run-all.sh
@@ -34,6 +34,10 @@ echo "Done, running python code..."
 cd ../run-py
 bash commands.dat
 
+cd ../lammps-input
+python3 get_data.py lammpsdata.dat
+python3 get_bond_coeff.py lammpsbondcoeff.dat
+
 echo "Done"
 
 echo "Testing..."
