0da5b8a2c4159ea277537366b1bdc8342ce2753d
BTC <btuchi@g.hmc.edu>
2024-12-08 16:59:07 -0800

    lammps-input generated.

--- run-all.sh
+++ run-all.sh
@@ -3,7 +3,18 @@ python3 align_mda.py
 
 echo "Aligned trajectories, running hENM refinement"
 
+echo "Running Perl code"
+
 cd ../run-pl
 bash commands.dat
+
+echo "Running Python code"
+
+cd ../run-py
+bash commands.dat
+
+cd ../lammps-input
+python3 get_data.py lammpsdata.dat
+python3 get_bond_coeff.py lammpsbondcoeff.dat
 
 echo "Done"
