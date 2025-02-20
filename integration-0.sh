f39fcdba6ed44b6a845318a75ffe588dec3300fc
Olivia Schleifer <oschleifer@hmc.edu>
2025-02-19 16:47:35 -0800

    updated lammps environment setup

--- integration-0.sh
+++ integration-0.sh
@@ -24,21 +24,20 @@ bash run-all.sh
 
 mkdir test-lammps-integration && cd test-lammps-integration
 
-source/usr/workspace/mummiusr/mummi-spack/spack/0.19/share/spack/setup-env.sh
+# source spack & load lammps
+source /usr/workspace/mummiusr/mummi-spack/spack/0.19/share/spack/setup-env.sh
 spack load lammps arch=$(spack arch)
 command -v lmp
 
+# source mummi
 source ~/.mummi/config.mummi.sh
 env | grep MUMMI
-source $MUMMI_APP/setup/setup.env.sh
-cp $MUMMI_RESOURCES/ucg/*
+cp $MUMMI_RESOURCES/ucg/* .
 
-source/usr/workspace/mummiusr/mummi-spack/spack/0.19/share/spack/setup-env.sh
-spack load lammps arch=$(spack arch)
-command -v lmp
-
+# create and run job
 lalloc 2 -W 30
-
 export OMP_NUM_THREADS=8
 jsrun -n1 -c1 lmp -v seed 1 -in lammps.in &
 
+# Each LAMMPS will write a trajectory file named ucg.traj.xyz
+wait
